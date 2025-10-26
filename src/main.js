b6b4118aa6fab000dae14b05f4d481fed1eef13e
aidanbeck <aidanmbeck@outlook.com>
2025-10-26 17:01:05 -0500

    restructured main.js & removed interaction.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
 import Dauber from './modules/dauber.js';
 import { Entity, Sprite, Circle, Runes, Lattice } from './modules/entities.js';
 import getCollisions from './modules/collider.js';
-import Bind from './modules/input.js';
 import { Position3D, Angle3D, Camera } from './modules/3d.js';
 import Scene from './modules/scene.js';
+import Bind from './modules/input.js';
 
 import './style.css';
 
-//Set up render context
+// CANVAS & DAUBER
 document.querySelector('#app').innerHTML = `<canvas></canvas>`; //create canvas
 let canvas = document.getElementsByTagName("canvas")[0];
 let daub = new Dauber(canvas);
 
-//Define image
-const playerImage = new Image();
-playerImage.src = "src/assets/dev.bmp";
 
+// SCENE & OBJECTS
 let scene = new Scene();
-scene.add( new Sprite(playerImage, 20, 20) );
 scene.add( new Circle(10, "white", 30, 30) );
 scene.add( new Runes("T", "green", 50, 50) );
+
+// Create Image
+const playerImage = new Image();
+playerImage.src = "src/assets/dev.bmp";
+scene.add( new Sprite(playerImage, 20, 20) );
 
 // Create Lattice
 let lattice = new Lattice(3,3,0,0);
@@ -27,6 +29,7 @@ lattice.setCell(0,0, {image: playerImage});
 lattice.setCell(1,1, {image: playerImage});
 lattice.setCell(2,2, {image: playerImage});
 scene.add(lattice);
+
 
 // MAIN LOOP
 setInterval(function() {
@@ -38,3 +41,29 @@ setInterval(function() {
     daub.frame(scene.objects, scene.renderQueue);
 
 }, 20);
+
+
+// INTERACTION
+let controlIndex = 0;
+let controllableObject = scene.objects[controlIndex];
+
+function switchControllableObject() {
+    event.preventDefault(); //prevent default tab behavior
+    controlIndex++;
+    if (controlIndex >= scene.objects.length) { controlIndex = 0; }
+    controllableObject = scene.objects[controlIndex];
+}
+
+function moveControllableObject(x, y) {
+    controllableObject.v.x += x*2;
+    controllableObject.v.y += y*2;
+}
+
+//Declare Binds
+new Bind("ArrowUp",   () => moveControllableObject( 0, -1 ) );
+new Bind("ArrowDown", () => moveControllableObject( 0,  1 ) );
+new Bind("ArrowLeft", () => moveControllableObject(-1,  0 ) );
+new Bind("ArrowRight",() => moveControllableObject( 1,  0 ) );
+new Bind("PageUp",    () => controllableObject.radius += 1 );
+new Bind("PageDown",  () =>{controllableObject.radius -= 1; if (controllableObject.radius < 1) { controllableObject.radius = 1; }} );
+new Bind("Tab",             switchControllableObject );
